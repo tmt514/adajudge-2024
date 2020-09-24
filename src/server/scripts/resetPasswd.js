86616eadfd24c36f3b22e02a31eb5c1428ec0bce
Your Name <you@example.com>
2020-09-24 01:46:03 -0700

    merge branch dsa to ada

--- src/server/scripts/resetPasswd.js
+++ src/server/scripts/resetPasswd.js
@@ -45,19 +45,19 @@ const resetUser = async (email, transporter) => {
   user.password = hashed;
   await user.save();
   const text = (
-    `Welcome to DSA2020, this email is to inform you that your DSA Judge account has been created.
+    `Welcome to ADA2020, this email is to inform you that your ADA Judge account has been created.
 Here is your account and temporary password. (You can change your password after logging in.)
 
 - Account: ${email}
 - Password: ${randPass}
 
-Head on to https://dsa.csie.org and try it!
+Head on to https://ada-judge.csie.ntu.edu.tw and try it!
 `);
 
   const mailOptions = {
-    from: '"DSA2020" <ta@dsa.csie.org >',
+    from: '"ADA2020" <ada-ta@csie.ntu.edu.tw >',
     to: email,
-    subject: '[DSA2020]Your DSA Judge Account',
+    subject: '[ADA2020]Your ADA Judge Account',
     text
   };
 
