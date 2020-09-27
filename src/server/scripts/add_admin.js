2db94adb6636f17bfcc5e2c6c086864d367732d8
robert1003 <b07902047@ntu.edu.tw>
2020-09-27 21:26:45 +0800

    change password gen

--- src/server/scripts/add_admin.js
+++ src/server/scripts/add_admin.js
@@ -1,12 +1,15 @@
 import './common';
 import User from '/model/user';
 import bcrypt from 'bcrypt';
+import randomString from 'randomstring';
 import {promisify} from 'bluebird';
 (async () => {
-  const hashed = await bcrypt.hash('this is password', 10);
+  const randPass = randomString.generate(10);
+  const hashed = await promisify(bcrypt.hash)(randPass, 10);
+  console.log("this is password:", randPass);
   const roles = ['admin'];
   const user = new User({
-    email: 'admin@abc.com',
+    email: 'admin@ada-judge.csie.ntu.edu.tw',
     password: hashed,
     roles: roles,
     meta: {
