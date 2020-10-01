e57c62c4c8d4239578f9d7e1eb7300c7be032c39
robert1003 <b07902047@ntu.edu.tw>
2020-10-01 16:21:15 +0800

    remove ssh-key(git) related config

--- src/server/model/user.js
+++ src/server/model/user.js
@@ -13,8 +13,8 @@ const userSchema = Schema({
     type: String,
     required: true
   },
-  ssh_key: String,
-  git_upload_key: String,
+  //ssh_key: String,
+  //git_upload_key: String,
   meta: {
     name: String,
     id: String
